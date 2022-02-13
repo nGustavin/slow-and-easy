3c314c70793bc8be7c4da4d88fee97f9c31d65fd
nGustavin <gust4f3rn@gmail.com>
2022-02-13 14:50:47 -0300

    random_number and type_check challenges added

---
+++ challenges/2/type_check.py
@@ -0,0 +1,9 @@
+# Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+
+# For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+
+def only_ints(x, y):
+    if type(x) == int and type(y) == int:
+        return True
+    else:
+        return False
